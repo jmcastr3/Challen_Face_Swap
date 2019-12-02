27c16e55260cc31cc3568458c014dc1515d4ddd0
June <jmcastr3@illinois.edu>
2019-12-02 16:45:17 -0600

    Camera Application

--- app/src/main/java/com/example/challenfaceswap/MainActivity.java
+++ app/src/main/java/com/example/challenfaceswap/MainActivity.java
@@ -2,13 +2,70 @@ package com.example.challenfaceswap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * If the user requests to take an image with their camera
+     */
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Button to take a picture. When clicked it opens the camera application on the phone.
+        Button takePic = findViewById(R.id.takePicture);
+        takePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        FirebaseVisionFaceDetectorOptions challenFace =
+                new FirebaseVisionFaceDetectorOptions.Builder()
+                        .setPerformanceMode(FirebaseVisionFaceDetectorOptions.ACCURATE)
+                        .setLandmarkMode(FirebaseVisionFaceDetectorOptions.ALL_LANDMARKS)
+                        .setClassificationMode(FirebaseVisionFaceDetectorOptions.ALL_CLASSIFICATIONS)
+                        .build();
+
+
     }
+
+    /**
+     * Makes an intent and starts an activity to take a picture.
+     */
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    /**
+     * This function runs when the user is done taking a picture. The image is converted into a bitmap.
+     * @param requestCode The code to request
+     * @param resultCode The resulting code
+     * @param data The intent received from the picture
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            //Get the picture as a bitmap and store it in the instance variable.
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        }
+    }
+
 }
